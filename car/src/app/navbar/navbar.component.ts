2b4e2493b4af764af656615a89e31ad7458015cc
Gloria Prantner <gloriaprantner@gmail.com>
2020-06-23 14:12:28 +0200

    finished click function

--- car/src/app/navbar/navbar.component.ts
+++ car/src/app/navbar/navbar.component.ts
@@ -7,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 	projectName:string;
+	clickCounter:number= 0;
+
   constructor() {
   this.projectName = "Car Rental"; }
+
+  countClick(){
+  	this.clickCounter = this.clickCounter + 10;
+  }
 
   ngOnInit() {
   }
